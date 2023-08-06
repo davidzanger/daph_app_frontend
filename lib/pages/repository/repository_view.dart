d21e4dd83646dece1079a89f190ccfc8a5600156
David Zanger <david.zanger@dlr.de>
2023-08-06 22:04:26 +0200

    Refactor and add navigation to card view.

--- lib/pages/repository/repository_view.dart
+++ lib/pages/repository/repository_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:langapp/cubits/mycards/mycards_bloc.dart';
 import 'package:langapp/models/data.dart';
+import 'package:langapp/pages/card_view/card_view.dart';
 
 class RepositoryView extends StatelessWidget {
   const RepositoryView({super.key});
@@ -36,40 +37,60 @@ class MyCardsListView extends StatelessWidget {
     return ListView.builder(
       itemCount: dataList.length,
       itemBuilder: (context, index) {
-        return Card(
-          child: ListTile(
-            title: WordListRow(wordList: dataList[index].words),
-            subtitle: Text(
-                "${dataList[index].textLength.toString()} words, ${dataList[index].difficulty.name}"),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                    onPressed: () {
-                      BlocProvider.of<MyCardsBloc>(context)
-                          .add(DeleteCard(card: dataList[index]));
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text('Card deleted from repository'),
-                          action: SnackBarAction(
-                            label: 'Undo',
-                            onPressed: () {
-                              BlocProvider.of<MyCardsBloc>(context).add(
-                                  InsertCard(
-                                      card: dataList[index], index: index));
-                            },
-                          ),
-                          duration: Duration(seconds: 2),
-                        ),
-                      );
-                    },
-                    icon: Icon(Icons.delete)),
-                IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
-              ],
-            ),
-          ),
-        );
+        return GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => CardView(data: dataList[index])));
+            },
+            child: VocabularyCard(data: dataList[index], index: index));
       },
+    );
+  }
+}
+
+class VocabularyCard extends StatelessWidget {
+  const VocabularyCard({
+    super.key,
+    required this.data,
+    required this.index,
+  });
+
+  final Data data;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: WordListRow(wordList: data.words),
+        subtitle: Text(
+            "${data.textLength.toString()} words, ${data.difficulty.name}"),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+                onPressed: () {
+                  BlocProvider.of<MyCardsBloc>(context)
+                      .add(DeleteCard(card: data));
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Card deleted from repository'),
+                      action: SnackBarAction(
+                        label: 'Undo',
+                        onPressed: () {
+                          BlocProvider.of<MyCardsBloc>(context)
+                              .add(InsertCard(card: data, index: index));
+                        },
+                      ),
+                      duration: Duration(seconds: 2),
+                    ),
+                  );
+                },
+                icon: Icon(Icons.delete)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+          ],
+        ),
+      ),
     );
   }
 }
